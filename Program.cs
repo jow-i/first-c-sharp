f9219dec6795b5628dc7ab08358fafcbad3e9465
jow-i <jessejowi@gmail.com>
2022-06-20 19:36:41 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int[] values={15,7,12,23,41,28,9,17,36};
+Console.WriteLine(" Using Break and Continue");
+foreach (int val in values)
+{
+    
+   /* if (val>=40)
+      { 
+           break;
+       }*/
+       if(val>=20 && val<=29){
+           continue;
+       }
+        Console.WriteLine($"val is currently {val}");
+}
